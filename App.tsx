356015a21c6c91b7fad3cc553641f948829286db
baritaha <135865393+baritaha@users.noreply.github.com>
2025-05-27 13:44:03 +0300

    add Task#17

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Task16 from './Tasks/Task#16'; // Import the Task Component
+import Task17 from './Tasks/Task#17';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,6 +13,7 @@ const App = () => {
   return (
     <View style={styles.container}>
       <Task16 />
+      <Task17/>
     </View>
   );
 };
---
+++ Tasks/Task#17.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const Task17 = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <View style={styles.container}>
+        <Text style={styles.text2}>Task #17</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setIsVisible(!isVisible)}
+      >
+        <Text style={styles.buttonText}>{isVisible ? "Hide" : "Show"}</Text>
+      </TouchableOpacity>
+
+      {isVisible && <Text style={styles.text}>abdel-bari altaha</Text>}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1,textAlign:'center',alignItems:'center'},
+  button: { backgroundColor: '#007AFF', padding: 10, borderRadius: 8 },
+  buttonText: { color: 'white', fontSize: 18, fontWeight: 'bold' },
+  text: { fontSize: 24, fontWeight: 'bold', marginTop: 10 },
+  text2: { fontSize: 18, margin: 10, backgroundColor: 'lightblue', padding: 10, borderRadius: 5, width: '100%', textAlign: 'center',fontWeight: 'bold' },
+});
+
+export default Task17;
