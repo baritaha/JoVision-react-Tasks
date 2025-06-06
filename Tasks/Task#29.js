8b1b29c9f44c6f8cabbd51ea9d23608d5f6db1fa
baritaha <135865393+baritaha@users.noreply.github.com>
2025-06-06 08:37:12 +0300

    add Task#29

---
+++ Tasks/Task#29.js
@@ -0,0 +1,137 @@
+import { useRef, useState } from 'react';
+import { Alert, Animated, Button, FlatList, Image, Modal, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { images } from '../assets/imagesArray/arraysOfimages';
+
+
+
+
+const Task29 = ()=>{
+    const [modalVisible, setModalVisible] = useState(false);
+    const [indexImage, setIndexImage] = useState(null);
+   const flatListRef = useRef(null);
+  const [isOnPressed, setIsOnPressed] = useState(false);
+    const scrollToIndex = () => {
+      setIsOnPressed(true);
+    const index = parseInt(indexImage, 10);
+    if (!isNaN(index) && index >= 0 && index < images.length) {
+      flatListRef.current?.scrollToIndex({ animated: true, index });
+    // setIndexImage(index);
+   // setIndexImage('');
+        setModalVisible(false);
+    }
+    else{
+        Alert.alert('Invalid Index', `Please enter a valid index between 0 and ${images.length - 1}.`);
+    }
+   //setIsOnPressed(false);
+  };
+    return(
+        <View style={styles.container}>
+            <Text style={styles.text2}>Task #29</Text>
+            <FlatList
+            ref={flatListRef}
+            data={images}
+            horizontal
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item, index}) =>
+                <View style={parseInt(indexImage, 10) === index && isOnPressed  ? styles.style1 : styles.style2}>
+                    <View>
+                    <Text style={styles.text}>
+                    {item.label}
+                    {parseInt(indexImage,10) === index && <Image source={require('../assets/images/check.png')} style={styles.icon} />}
+                  </Text>
+            </View>
+                    <Image source={item.src} style={styles.image}/>
+                </View>
+            }
+            />
+            <Button title="Open Modal" onPress={()=>{setModalVisible(true); setIsOnPressed(false); setIndexImage('');}}/>
+            <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={()=>setModalVisible(false)}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                       <Text style={styles.modalTitle}>Enter Index to scroll image</Text>
+                       <TextInput style={styles.input} placeholder="Type Index ..."
+                       value={indexImage} onChangeText={setIndexImage}/>
+                       <Text style={styles.text}>Enter index from 0 to {images.length - 1}</Text>
+                       <View style={styles.btnGroup}>
+                        <Button title="Close Modal" onPress={()=>setModalVisible(false)}/>
+                        <TouchableOpacity style={styles.button} onPress={scrollToIndex}>
+                         <Text style={styles.textScroll}>Scroll to Index</Text>
+                        </TouchableOpacity>
+                        </View>
+                    </View>
+
+                </View>
+            </Modal>
+        </View>
+    );
+};
+const styles = {
+    container:{flex:1, justifyContent:'center', alignItems:'center'},
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent:{
+        width: '80%',
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    input: {
+        width: '100%',
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 20,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+    },
+    text:{fontSize:20, fontWeight:'bold',justifyContent:'center',textAlign:'center',margin:10,
+        marginBottom:-10,backgroundColor:'aqua',padding:10,color:'black',borderTopLeftRadius:15,
+        borderTopRightRadius:15},
+    text2: {fontSize: 18, margin: 10, backgroundColor: 'lightblue', padding: 10, borderRadius: 5, width: '100%', textAlign: 'center',fontWeight: 'bold'},
+    btnGroup:{flexDirection: 'row', justifyContent: 'space-between', width: '100%', marginTop: 20, paddingHorizontal: 20},
+    button: {
+        backgroundColor: 'blue',
+        padding: 10,
+        borderRadius: 5,
+        marginHorizontal: 10,
+    },
+    image:{ width: 200, height: 200, margin: 10,borderBottomLeftRadius:15, borderBottomRightRadius:15},
+    style1:{
+          margin:20,
+        borderWidth: 4,
+        borderColor: 'green',
+        borderRadius: 10,
+        transform:[{ scaleY: 1.09 }],
+        transition: {
+            duration: 600,
+        },
+    },
+    style2:{
+        margin:20,
+        borderWidth: 2,
+        borderColor: 'gray',
+        borderRadius: 10,
+    },
+    textScroll:{color:'white', fontWeight:'bold'},
+  icon: {
+    marginLeft: 10,
+    width: 20,
+    height: 20,
+  },
+    };
+export default Task29;
