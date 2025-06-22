83c7c019913e39460869195d3d4865a5250f38bf
baritaha <135865393+baritaha@users.noreply.github.com>
2025-06-22 11:27:47 +0300

    add task#37

--- Tasks/Task#36.js
+++ Tasks/Task#36.js
@@ -1,6 +1,6 @@
 // Task36.js
-import React from 'react';
-import { ScrollView, Text, StyleSheet, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { ScrollView, Text, StyleSheet, View, RefreshControl } from 'react-native';
 
 const generateRandomWord = (length) => {
   let result = '';
@@ -10,27 +10,51 @@ const generateRandomWord = (length) => {
   }
   return result;
 };
-
-const Task36 = () => {
-  const randomWords = Array.from({ length: 100 }, (_, i) => ({
+const generateWords = () => {
+  return Array.from({ length: 100 }, (_, i) => ({
     id: i + 1,
-    word: generateRandomWord(8 + Math.floor(Math.random() * 5)), // words 8–12 letters
+    word: generateRandomWord(8 + Math.floor(Math.random() * 5)),
   }));
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Task #36 - Scrollable Words</Text>
-      <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={true}>
-        {randomWords.map(({ id, word }) => (
-          <Text key={id} style={styles.wordText}>
-            #{id}: {word}
-          </Text>
-        ))}
-      </ScrollView>
-    </View>
-  );
 };
 
+const Task36 = () => {
+  const [randomWords,setRandomWords] = useState(generateWords());
+  const [refresh,setRefreshing] = useState(false);
+
+  const onRefresh = React.useCallback(()=>{
+    console.log(refresh);
+    setRefreshing(true);
+    setTimeout(() => {
+      setRandomWords(generateWords());
+      setRefreshing(true);
+    }, 2000);
+  },[refresh]);
+  // useEffect(() => {
+  //   console.log(refresh);
+  // }, [refresh]);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Task #36/37 - Scrollable Words</Text>
+      <Text style={styles.textTitle}>Pull down to see RefreshControl indicator</Text>
+     <View style={styles.scrollWrapper}>
+       <ScrollView style={styles.scrollView}
+         refreshControl={
+         <RefreshControl refreshing={refresh} onRefresh={onRefresh}/>
+      }
+      showsVerticalScrollIndicator={true}
+      persistentScrollbar={true}
+       nestedScrollEnabled={true}
+      >
+      {randomWords.map(({ id, word }) => (
+      <Text key={id} style={styles.wordText}>
+        #{id}: {word}
+      </Text>
+    ))}
+      </ScrollView>
+    </View>
+</View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -45,15 +69,15 @@ heading: {fontSize: 18, margin: 10, backgroundColor: 'lightblue',
  scrollView: {
   marginTop:10,
   width:'100%',
-  maxWidth:'70%',
+  maxWidth:'100%',
   padding: 10,
-  maxHeight: 330,
+  maxHeight: 380,
   marginRight: 35,
   backgroundColor: 'skyblue',
   borderRadius: 6,
   paddingHorizontal: 19,
-},
 
+},
   wordText: {
     fontSize: 20,
     marginVertical: 8,
@@ -63,6 +87,13 @@ heading: {fontSize: 18, margin: 10, backgroundColor: 'lightblue',
     color:'chocolate',
     fontWeight:'bold',
   },
+  scrollWrapper: {
+  width: '100%',
+  maxWidth: '70%',
+  marginRight: 35,
+  backgroundColor: 'skyblue',
+  borderRadius: 6,
+  },
+  textTitle:{fontSize:18,color:'blue',fontWeight:'bold',marginBottom:10},
 });
-
 export default Task36;
